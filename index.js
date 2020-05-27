300a0bf1bcfb48296452114113a0c8ef7144dfc8
Ken Sia <technical@threestepsbusiness.com>
2020-05-27 22:47:47 +0800

    Add Validation for Company Customer's Primary Quote Contact

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const simproUrl = process.env.SIMPRO_URL;
 const simproApiKey = process.env.SIMPRO_API_KEY;
 const integromatHook = process.env.INTEGROMAT_HOOK;
 
+// For Local development testing
+// const simproUrl = 'https://aptplumbing.simprosuite.com';
+// const simproApiKey = '4d77a9138a51b7414707e0ba8bd5adb4318c3642';
+// const integromatHook = 'https://hook.integromat.com/vrr2f1g4tmj6uktlcr9f4ux9ou7v3m5y'
+// getQuotes();
+
 exports.handler = (event, context, callback) => {
     getQuotes()
 };
@@ -150,14 +156,48 @@ function getCustomerMainContact (customer, quote) {
     var mainContact = mainContacts[0]
 
     if(mainContact) { //check if has contacts? - yes? save first contact as first and last name, orgname as company name, and email as company EMAIL
-      customer.GivenName = mainContact.GivenName
-      customer.FamilyName = mainContact.FamilyName
-      customer.CellPhone = mainContact.CellPhone
-      postContact(customer, quote)
+      // loop thru all customer contacts to check which is the main quote contact
+      mainContacts.forEach(function(contact) {
+        getCustomerQuoteContact(contact, customer, quote)
+      });
     } else { // - no? save contact as company firstname and last name, orgname as copmpany name, email as company email
       customer.GivenName = customer.CompanyName
       customer.CellPhone = customer.Phone
+      // console.log(customer, quote)
       postContact(customer, quote)
+    }
+  });
+}
+
+function getCustomerQuoteContact (contact, customer, quote) {
+  var request = require("request");
+
+  var options = {
+    method: 'GET',
+    url: simproUrl + '/api/v1.0/companies/0/customers/' + customer.ID + '/contacts/' + contact.ID,
+    headers:
+    {
+      Authorization: 'Bearer ' + simproApiKey,
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    var contact = JSON.parse(body);
+
+    if(contact.PrimaryQuoteContact) { //check if contact is Primary Quote Contact - yes? save first contact as first and last name, orgname as company name, and email as company EMAIL
+      customer.GivenName = contact.GivenName
+      customer.FamilyName = contact.FamilyName
+      if(contact.Email) customer.Email = contact.Email
+      customer.CellPhone = contact.CellPhone
+      customer.PrimaryQuoteContact = contact.PrimaryQuoteContact
+      // console.log(customer, quote)
+      postContact(customer, quote)
+    } else { // - no? save contact as company firstname and last name, orgname as copmpany name, email as company email
+      console.log('-- Contact is not a Primary Quote Contact --')
     }
   });
 }
@@ -183,6 +223,7 @@ function getCustomerIndividual (id, quote) {
 
     if(customerIndividual.Email) {
       if(validEmail(customerIndividual.Email)) {
+        // console.log(customer, quote)
         postContact(customerIndividual, quote)
       } else {
         console.log('-- Customer has an invalid email address --')
